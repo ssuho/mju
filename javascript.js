16053a60207856976067b5755540e95ca22dabe7
M3MENTO\m3men <devsuhokim@gmail.com>
2022-08-25 21:29:52 +0900

    edited image name

--- javascript.js
+++ javascript.js
@@ -26,25 +26,25 @@ function check_class(){
 
   // [인캠]본관
     else if(1200 > n && n >= 1100){
-    document.getElementById('result').innerHTML='<img src="./image/1.jpg" width="80%" alt="[인캠] 종합관"></img><br><br>[인캠] 종합관 (구 본관, 1번 건물)<br><br><span style="font-size: 24px;">1층</span>';
+    document.getElementById('result').innerHTML='<img src="./image/S1.jpg" width="80%" alt="[인캠] 종합관"></img><br><br>[인캠] 종합관 (구 본관, 1번 건물)<br><br><span style="font-size: 24px;">1층</span>';
   } else if(1300 > n && n >= 1200){
-    document.getElementById('result').innerHTML='<img src="./image/1.jpg" width="80%" alt="[인캠] 종합관"></img><br><br>[인캠] 종합관 (구 본관, 1번 건물)<br><br><span style="font-size: 24px;">2층 교양, 영어 강의실</span>';
+    document.getElementById('result').innerHTML='<img src="./image/S1.jpg" width="80%" alt="[인캠] 종합관"></img><br><br>[인캠] 종합관 (구 본관, 1번 건물)<br><br><span style="font-size: 24px;">2층 교양, 영어 강의실</span>';
   } else if(1400 > n && n >=1300){
-    document.getElementById('result').innerHTML='<img src="./image/1.jpg" width="80%" alt="[인캠] 종합관"></img><br><br>[인캠] 종합관 (구 본관, 1번 건물)<br><br><span style="font-size: 24px;">3층 교양, ICT 강의실</span>';
+    document.getElementById('result').innerHTML='<img src="./image/S1.jpg" width="80%" alt="[인캠] 종합관"></img><br><br>[인캠] 종합관 (구 본관, 1번 건물)<br><br><span style="font-size: 24px;">3층 교양, ICT 강의실</span>';
   } else if(1500 > n && n >=1400){
-    document.getElementById('result').innerHTML='<img src="./image/1.jpg" width="80%" alt="[인캠] 종합관"></img><br><br>[인캠] 종합관 (구 본관, 1번 건물)<br><br><span style="font-size: 24px;">4층 경제, 청지, 아동 강의실</span>';
+    document.getElementById('result').innerHTML='<img src="./image/S1.jpg" width="80%" alt="[인캠] 종합관"></img><br><br>[인캠] 종합관 (구 본관, 1번 건물)<br><br><span style="font-size: 24px;">4층 경제, 청지, 아동 강의실</span>';
   } else if(1600 > n && n >=1500){
-    document.getElementById('result').innerHTML='<img src="./image/1.jpg" width="80%" alt="[인캠] 종합관"></img><br><br>[인캠] 종합관 (구 본관, 1번 건물)<br><br><span style="font-size: 24px;">5층 법대 강의실</span>';
+    document.getElementById('result').innerHTML='<img src="./image/S1.jpg" width="80%" alt="[인캠] 종합관"></img><br><br>[인캠] 종합관 (구 본관, 1번 건물)<br><br><span style="font-size: 24px;">5층 법대 강의실</span>';
   } else if(1700 > n && n >=1600){
-    document.getElementById('result').innerHTML='<img src="./image/1.jpg" width="80%" alt="[인캠] 종합관"></img><br><br>[인캠] 종합관 (구 본관, 1번 건물)<br><br><span style="font-size: 24px;">6층 사과대, 법대 강의실</span>';
+    document.getElementById('result').innerHTML='<img src="./image/S1.jpg" width="80%" alt="[인캠] 종합관"></img><br><br>[인캠] 종합관 (구 본관, 1번 건물)<br><br><span style="font-size: 24px;">6층 사과대, 법대 강의실</span>';
   } else if(1800 > n && n >=1700){
-    document.getElementById('result').innerHTML='<img src="./image/1.jpg" width="80%" alt="[인캠] 종합관"></img><br><br>[인캠] 종합관 (구 본관, 1번 건물)<br><br><span style="font-size: 24px;">7층 인문 강의실</span>';
+    document.getElementById('result').innerHTML='<img src="./image/S1.jpg" width="80%" alt="[인캠] 종합관"></img><br><br>[인캠] 종합관 (구 본관, 1번 건물)<br><br><span style="font-size: 24px;">7층 인문 강의실</span>';
   } else if(1900 > n && n >=1800){
-    document.getElementById('result').innerHTML='<img src="./image/1.jpg" width="80%" alt="[인캠] 종합관"></img><br><br>[인캠] 종합관 (구 본관, 1번 건물)<br><br><span style="font-size: 24px;">8층 인문 강의실</span>';
+    document.getElementById('result').innerHTML='<img src="./image/S1.jpg" width="80%" alt="[인캠] 종합관"></img><br><br>[인캠] 종합관 (구 본관, 1번 건물)<br><br><span style="font-size: 24px;">8층 인문 강의실</span>';
   } else if(2000 > n && n >=1900){
-    document.getElementById('result').innerHTML='<img src="./image/1.jpg" width="80%" alt="[인캠] 종합관"></img><br><br>[인캠] 종합관 (구 본관, 1번 건물)<br><br><span style="font-size: 24px;">9층 인문 강의실</span>';
+    document.getElementById('result').innerHTML='<img src="./image/S1.jpg" width="80%" alt="[인캠] 종합관"></img><br><br>[인캠] 종합관 (구 본관, 1번 건물)<br><br><span style="font-size: 24px;">9층 인문 강의실</span>';
   } else if(12000 > n && n >=11000){
-    document.getElementById('result').innerHTML='<img src="./image/1.jpg" width="80%" alt="[인캠] 종합관"></img><br><br>[인캠] 종합관 (구 본관, 1번 건물)<br><br><span style="font-size: 24px;">10층 대강당, 대학원</span>';
+    document.getElementById('result').innerHTML='<img src="./image/S1.jpg" width="80%" alt="[인캠] 종합관"></img><br><br>[인캠] 종합관 (구 본관, 1번 건물)<br><br><span style="font-size: 24px;">10층 대강당, 대학원</span>';
   }
 
   
@@ -72,25 +72,25 @@ function check_class(){
 
   // [인캠]경상관
     else if(4100 > n && n >=4000){
-    document.getElementById('result').innerHTML='<img src="./image/4.jpg" width="80%" alt="[인캠] 국제관"></img><br><br>[인캠] 국제관 (구 경상관, 4번 건물)<br><br><span style="font-size: 24px;">지하 1층</span>';
+    document.getElementById('result').innerHTML='<img src="./image/S4.jpg" width="80%" alt="[인캠] 국제관"></img><br><br>[인캠] 국제관 (구 경상관, 4번 건물)<br><br><span style="font-size: 24px;">지하 1층</span>';
   } else if(4200 > n && n >=4100){
-    document.getElementById('result').innerHTML='<img src="./image/4.jpg" width="80%" alt="[인캠] 국제관"></img><br><br>[인캠] 국제관 (구 경상관, 4번 건물)<br><br><span style="font-size: 24px;">1층</span>';
+    document.getElementById('result').innerHTML='<img src="./image/S4.jpg" width="80%" alt="[인캠] 국제관"></img><br><br>[인캠] 국제관 (구 경상관, 4번 건물)<br><br><span style="font-size: 24px;">1층</span>';
   } else if(4300 > n && n >=4200){
-    document.getElementById('result').innerHTML='<img src="./image/4.jpg" width="80%" alt="[인캠] 국제관"></img><br><br>[인캠] 국제관 (구 경상관, 4번 건물)<br><br><span style="font-size: 24px;">2층</span>';
+    document.getElementById('result').innerHTML='<img src="./image/S4.jpg" width="80%" alt="[인캠] 국제관"></img><br><br>[인캠] 국제관 (구 경상관, 4번 건물)<br><br><span style="font-size: 24px;">2층</span>';
   } else if(4400 > n && n >=4300){
-    document.getElementById('result').innerHTML='<img src="./image/4.jpg" width="80%" alt="[인캠] 국제관"></img><br><br>[인캠] 국제관 (구 경상관, 4번 건물)<br><br><span style="font-size: 24px;">3층</span>';
+    document.getElementById('result').innerHTML='<img src="./image/S4.jpg" width="80%" alt="[인캠] 국제관"></img><br><br>[인캠] 국제관 (구 경상관, 4번 건물)<br><br><span style="font-size: 24px;">3층</span>';
   } else if(4500 > n && n >=4400){
-    document.getElementById('result').innerHTML='<img src="./image/4.jpg" width="80%" alt="[인캠] 국제관"></img><br><br>[인캠] 국제관 (구 경상관, 4번 건물)<br><br><span style="font-size: 24px;">4층</span>';
+    document.getElementById('result').innerHTML='<img src="./image/S4.jpg" width="80%" alt="[인캠] 국제관"></img><br><br>[인캠] 국제관 (구 경상관, 4번 건물)<br><br><span style="font-size: 24px;">4층</span>';
   } else if(4600 > n && n >=4500){
-    document.getElementById('result').innerHTML='<img src="./image/4.jpg" width="80%" alt="[인캠] 국제관"></img><br><br>[인캠] 국제관 (구 경상관, 4번 건물)<br><br><span style="font-size: 24px;">5층</span>';
+    document.getElementById('result').innerHTML='<img src="./image/S4.jpg" width="80%" alt="[인캠] 국제관"></img><br><br>[인캠] 국제관 (구 경상관, 4번 건물)<br><br><span style="font-size: 24px;">5층</span>';
   } else if(4700 > n && n >=4600){
-    document.getElementById('result').innerHTML='<img src="./image/4.jpg" width="80%" alt="[인캠] 국제관"></img><br><br>[인캠] 국제관 (구 경상관, 4번 건물)<br><br><span style="font-size: 24px;">6층</span>';
+    document.getElementById('result').innerHTML='<img src="./image/S4.jpg" width="80%" alt="[인캠] 국제관"></img><br><br>[인캠] 국제관 (구 경상관, 4번 건물)<br><br><span style="font-size: 24px;">6층</span>';
   } else if(4800 > n && n >=4700){
-    document.getElementById('result').innerHTML='<img src="./image/4.jpg" width="80%" alt="[인캠] 국제관"></img><br><br>[인캠] 국제관 (구 경상관, 4번 건물)<br><br><span style="font-size: 24px;">7층</span>';
+    document.getElementById('result').innerHTML='<img src="./image/S4.jpg" width="80%" alt="[인캠] 국제관"></img><br><br>[인캠] 국제관 (구 경상관, 4번 건물)<br><br><span style="font-size: 24px;">7층</span>';
   } else if(4900 > n && n >=4800){
-    document.getElementById('result').innerHTML='<img src="./image/4.jpg" width="80%" alt="[인캠] 국제관"></img><br><br>[인캠] 국제관 (구 경상관, 4번 건물)<br><br><span style="font-size: 24px;">8층</span>';
+    document.getElementById('result').innerHTML='<img src="./image/S4.jpg" width="80%" alt="[인캠] 국제관"></img><br><br>[인캠] 국제관 (구 경상관, 4번 건물)<br><br><span style="font-size: 24px;">8층</span>';
   } else if(4999 > n && n >=4900){
-    document.getElementById('result').innerHTML='<img src="./image/4.jpg" width="80%" alt="[인캠] 국제관"></img><br><br>[인캠] 국제관 (구 경상관, 4번 건물)<br><br><span style="font-size: 24px;">9층</span>';
+    document.getElementById('result').innerHTML='<img src="./image/S4.jpg" width="80%" alt="[인캠] 국제관"></img><br><br>[인캠] 국제관 (구 경상관, 4번 건물)<br><br><span style="font-size: 24px;">9층</span>';
   }
 
   //[자캠] 제5공학관 (1)
@@ -109,7 +109,7 @@ function check_class(){
   
   // [인캠] 행정동
   else if(5520 > n && n >= 5510){
-    document.getElementById('result').innerHTML='<img src="./image/5.jpg" width="80%" alt="행정동"></img><br><br>[인캠] 행정동 (5번 건물)<br><br><span style="font-size: 24px;">5층 강당</span>';
+    document.getElementById('result').innerHTML='<img src="./image/S5.jpg" width="80%" alt="행정동"></img><br><br>[인캠] 행정동 (5번 건물)<br><br><span style="font-size: 24px;">5층 강당</span>';
   }
 
   //[자캠] 제5공학관 (2)
@@ -164,7 +164,7 @@ function check_class(){
 
   // [인캠] 방목학술정보관
   else if(n == 9114){
-    document.getElementById('result').innerHTML='<img src="./image/9.jpg" width="80%" alt="방목학술정보관"></img><br><br>[인캠] 방목학술정보관 (도서관, 9번 건물)<br><br><span style="font-size: 24px;">1층 국제회의장</span>';
+    document.getElementById('result').innerHTML='<img src="./image/S9.jpg" width="80%" alt="방목학술정보관"></img><br><br>[인캠] 방목학술정보관 (도서관, 9번 건물)<br><br><span style="font-size: 24px;">1층 국제회의장</span>';
   }
 
   //[자캠] 함박관
@@ -186,13 +186,13 @@ function check_class(){
 
   // [인캠] MCC
     else if(10200 > n && n >=10100){
-    document.getElementById('result').innerHTML='<img src="./image/10.jpg" width="80%" alt="[인캠] MCC"></img><br><br>[인캠] MCC (10번 건물)<br><br><span style="font-size: 24px;">베리타스홀 1층</span>';
+    document.getElementById('result').innerHTML='<img src="./image/S10.jpg" width="80%" alt="[인캠] MCC"></img><br><br>[인캠] MCC (10번 건물)<br><br><span style="font-size: 24px;">베리타스홀 1층</span>';
   } else if(10300 > n && n >=10200){
-    document.getElementById('result').innerHTML='<img src="./image/10.jpg" width="80%" alt="[인캠] MCC"></img><br><br>[인캠] MCC (10번 건물)<br><br><span style="font-size: 24px;">베리타스홀 2층</span>';
+    document.getElementById('result').innerHTML='<img src="./image/S10.jpg" width="80%" alt="[인캠] MCC"></img><br><br>[인캠] MCC (10번 건물)<br><br><span style="font-size: 24px;">베리타스홀 2층</span>';
   } else if(10400 > n && n >=10300){
-    document.getElementById('result').innerHTML='<img src="./image/10.jpg" width="80%" alt="[인캠] MCC"></img><br><br>[인캠] MCC (10번 건물)<br><br><span style="font-size: 24px;">베리타스홀 3층</span>';
+    document.getElementById('result').innerHTML='<img src="./image/S10.jpg" width="80%" alt="[인캠] MCC"></img><br><br>[인캠] MCC (10번 건물)<br><br><span style="font-size: 24px;">베리타스홀 3층</span>';
   } else if(10500 > n && n >=10400){
-    document.getElementById('result').innerHTML='<img src="./image/10.jpg" width="80%" alt="[인캠] MCC"></img><br><br>[인캠] MCC (10번 건물)<br><br><span style="font-size: 24px;">베리타스홀 4층</span>';
+    document.getElementById('result').innerHTML='<img src="./image/S10.jpg" width="80%" alt="[인캠] MCC"></img><br><br>[인캠] MCC (10번 건물)<br><br><span style="font-size: 24px;">베리타스홀 4층</span>';
   }
   
   //[자캠] 디자인조형센터
